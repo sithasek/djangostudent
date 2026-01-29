15b1429d62ace1931db2b41e4c4836e0149aad0e
sithasek <39076702+sithasek@users.noreply.github.com>
2026-01-29 13:49:41 +0700

    Update urls.py

--- apicumt/urls.py
+++ apicumt/urls.py
@@ -3,6 +3,7 @@ from rest_framework.urls import path
 
 urlpatterns = [
     path('students/', views.get_students, name='get_students'),
+    path('students/login/', views.login_student, name='login_student'),
     path('students/<int:pk>/', views.get_student_details, name='get_student_details'),
     path('students/create/', views.create_student, name='create_student'),
     path('students/update/<int:pk>/', views.update_student, name='update_student'),
