47a49180bec057fa6cde36f8f79e85ea6d1b34fc
sithasek <39076702+sithasek@users.noreply.github.com>
2026-01-30 14:37:24 +0700

    Add login4c2 path to URLs

--- api4c2/urls.py
+++ api4c2/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('student4c2/', views.student4c2_list_create, name='student4c2-list-create'),
     path('student4c2/<int:pk>/', views.student4c2_detail, name='student4c2-detail'),
+    path('login4c2/', views.login4c2, name='login4c2'),
 ]
