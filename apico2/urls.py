01fd007b94cbd4662b927e0025df96196cdea5a7
sithasek <39076702+sithasek@users.noreply.github.com>
2026-01-30 13:50:39 +0700

    Add update, delete, and login paths for studentc02

--- apico2/urls.py
+++ apico2/urls.py
@@ -4,4 +4,7 @@ urlpatterns = [
     path('studentc02/', views.get_studentc02, name='studentc02'),
     path('studentc02/<int:pk>/', views.get_studentco2_details, name='studentc02-details'),
     path('studentc02/create/', views.create_studentco2, name='create-studentc02'),
+    path('studentc02/update/<int:pk>/', views.update_studentco2, name='update-studentc02'),
+    path('studentc02/delete/<int:pk>/', views.delete_studentco2, name='delete-studentc02'),
+    path('studentc02/login/', views.login_studentco2, name='login-studentc02'),
 ]
